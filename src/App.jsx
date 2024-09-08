387d2f0edad312b9b046d023fabca6cfd861ee09
leenaabdalrahman <1l;2e3e4n5@>
2024-09-08 19:44:34 +0300

    first commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,10 @@
 import React from 'react'
-import Navbar from './components/Navbar.jsx'
-import Contact from './components/Contact.jsx'
-import 'bootstrap/dist/css/bootstrap.min.css'
-import 'bootstrap/dist/js/bootstrap.bundle.js'
-import Player from './components/Player.jsx'
+import Navbar from './components/Navbar.jsx';
+import Contact from './components/Contact.jsx';
+import Player from './components/Player.jsx';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap/dist/js/bootstrap.bundle.js';
+
 
 export default function App() {
   return (
